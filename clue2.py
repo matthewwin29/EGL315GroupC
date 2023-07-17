ab3fadb257d6c55f757775eecf16a25cae057a59
matthewwin29 <116146371+matthewwin29@users.noreply.github.com>
2023-07-17 19:17:32 +0800

    Create clue2.py
    blue pi's clue2

---
+++ clue2.py
@@ -0,0 +1,24 @@
+import RPi.GPIO as GPIO
+import time
+import socket
+import subprocess
+
+
+
+
+
+TCP_IP = '192.168.254.11'
+TCP_PORT = 5612
+BUFFER_SIZE = 1024
+
+
+MESSAGE4 = b"Clue2"
+
+
+
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.connect((TCP_IP,TCP_PORT))
+
+
+s.send(MESSAGE4)
+
