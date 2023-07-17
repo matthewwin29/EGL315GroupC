0f30ead290e60ed6b81015f9009a64b5c396d086
matthewwin29 <116146371+matthewwin29@users.noreply.github.com>
2023-07-17 19:19:09 +0800

    Create clue4.py
    blue pi clue4

---
+++ clue4.py
@@ -0,0 +1,25 @@
+import RPi.GPIO as GPIO
+import time
+import socket
+import subprocess
+
+
+
+
+
+TCP_IP = '192.168.254.11'
+TCP_PORT = 5612
+BUFFER_SIZE = 1024
+
+
+MESSAGE5 = b"Clue4"
+
+
+
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.connect((TCP_IP,TCP_PORT))
+
+
+s.send(MESSAGE5)
+time.sleep(1)
+
